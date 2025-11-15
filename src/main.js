f159af7600854a9ba23ac688483ffaff86e507ec
Manishbhai9350 <keshraram294@gmail.com>
2025-11-15 18:43:25 +0530

    Cards Smoothed

--- src/main.js
+++ src/main.js
@@ -4,7 +4,24 @@ import './style.css'
 
 gsap.registerPlugin(ScrollTrigger)
 
-const CardRotations = [0, 0, 0]
+const CardRotations = [10, 0, -10]
+const Positions = [
+  {
+    left: '5%',
+    right: 'auto',
+    transform: 'rotate(-85deg) translate(-50%,-50%)'
+  },
+  {
+    left: '50%',
+    right: 'auto',
+    transform: 'rotate(0deg) translate(-50%,-50%)'
+  },
+  {
+    left: 'auto',
+    right: '5%',
+    transform: 'rotate(85deg) translate(-50%,-50%)'
+  },
+]
 
 function InRange(val, range = [0, 0]) {
   if (val >= range[0] && val < range[1]) return true;
@@ -16,13 +33,10 @@ window.addEventListener('DOMContentLoaded', function () {
   const cards = gsap.utils.toArray('.card');
   const scrollText = document.querySelector('.scroll-text')
 
-  gsap.set(cards, {
-    y: 1000,
-  })
 
-  cards.forEach((card,i) => {
-    gsap.set(card,{
-      rotate:CardRotations[i]
+  cards.forEach((card, i) => {
+    gsap.set(card, {
+      transform: `rotate(${CardRotations[i]}deg) translate(-50%,950%)`
     })
   })
 
@@ -38,52 +52,43 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   })
 
-  ScrollTrigger.create({
-    trigger: '.cards',
-    start: `top -${1 * innerHeight}`,
-    end: `+=${4 * innerHeight}`,
-    onUpdate(me) {
-      const prog = me.progress
-
-      cards.forEach((card, i) => {
-        let range = [1 / 3 * i, 1 / 3 * (i + 1)]
-        if (InRange(prog, range)) {
-          const CardProg = (prog - range[0]) / (1 / 3)
-          gsap.set(card, {
-            y: 1000 - 1000 * CardProg
-          })
-        } else if (prog > range[1]) {
-          gsap.set(card, {
-            y: 0
-          })
-        }
-        if (prog == 1) {
-          gsap.set(cards, { y: 0 })
-        }
-        else if (prog == 0) {
-          gsap.set(cards, { y: 1000 })
-        }
-      })
-
-    },
-    onLeave() {
-      gsap.to(cards, { y: 0 })
+  const TL = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.cards',
+      start: `top -${1 * innerHeight}`,
+      end: `+=${4 * innerHeight}`,
+      scrub: true
     }
   })
 
+  TL.to(cards, {
+    transform: 'translate(-50%,-50%) rotate(0deg)',
+    stagger: {
+      each: 0.5,
+      overlap: 0  // ensures no overlap
+    }
+  });
 
-  cards.forEach((card,i) => {
-    gsap.to(card, {
-      rotate: 0,
-      x:i == 0 ? -220 : i == 2 ? 220 : 0,
-      scrollTrigger: {
-        trigger: '.cards',
-        start: `top -${4 * innerHeight}`,
-        end: `+=${6 * innerHeight}`,
-        scrub:2
-      }
-    })
+  const TL2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.cards',
+      start: `top -${4.5 * innerHeight}`,
+      end: `+=${7 * innerHeight}`,
+      scrub: true
+    }
   })
+  TL2.to(cards[0], {
+    left:'-5%',
+    transform:'translate(0%-50%)'
+  }, '<')
+  TL2.to(cards[2], {
+    left:'105%',
+    // right:'0',
+    transform:'translate(100%-50%)'
+  }, '<')
+
+
+
 
 
   ScrollTrigger.create({
